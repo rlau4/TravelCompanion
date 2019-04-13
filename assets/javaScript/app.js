6ec86113a38c337b7cbae850e5aecc8703567dae
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-04-13 13:30:29 -0400

    submit moved/id changed java started

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,5 +1,14 @@
+var popularArr = [];
+var currentDest = "HERE";
+
 // Google Places API
     $(function activatePlacesSearch(){
-        var input = document.getElementById('searchBar');
+        var input = document.getElementById('search-bar');
         var autocomplete = new google.maps.places.Autocomplete(input);
     });
+
+// function searchInput() {
+    $("#search").on("click", function() {
+        currentDest = $("#search-bar").val();
+    });
+// };
