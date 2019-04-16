b710f13291f3a1ad8a0ba932c580b0d0b7d64f5d
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-04-15 20:48:55 -0400

    logging to firebase appropraitely

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -15,17 +15,56 @@ firebase.initializeApp(config);
 data = firebase.database();
 
 
-data.ref() {
+var inputCity;
+var inputState;
+var inputCountry;
+var inputLat;
+var inputLong;
 
-}
 // Google Places API
 $(function activatePlacesSearch(){
     var input = document.getElementById('searchBar');
     var autocomplete = new google.maps.places.Autocomplete(input);
 });
 
-// function searchInput() {
-$("#search").on("click", function() {
+function searchInput() {
     currentDest = $("#searchBar").val();
-});
-// };
+    $("#searchBar").text('');
+    splitCurrentDest = currentDest.split(' ');
+    console.log(splitCurrentDest);
+ 
+
+    if (splitCurrentDest.length == 3) {
+        inputCity = splitCurrentDest[0];
+        inputState = splitCurrentDest[1];
+        inputCountry = splitCurrentDest[2];
+    } else if (splitCurrentDest.length == 2) {
+        inputCity = splitCurrentDest[0];
+        inputState = null;
+        inputCountry = splitCurrentDest[1];
+
+    } else if (splitCurrentDest.length == 4) {
+        inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1];
+        inputState = splitCurrentDest[2];
+        inputCountry = splitCurrentDest[3];
+    } else if (splitCurrentDest.length == 5) {
+       inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1] + " " + splitCurrentDest[2];
+       inputState = splitCurrentDest[3];
+       inputCountry = splitCurrentDest[4];
+    }
+    newSearch = {
+        city: inputCity,
+        state: inputState,
+        country: inputCountry,
+        // latitude: inputLat,
+        // longitude: inputLong
+    }
+    
+
+    data.ref().push(newSearch);
+    console.log("City: " + newSearch.city);
+    console.log("State " + newSearch.state);
+    console.log("country:" + newSearch.country);
+};
+
+$("#search").on("click", searchInput);
