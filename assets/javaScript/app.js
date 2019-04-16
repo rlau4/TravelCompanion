525f2d503e9e7245146d3005479c56fdef23628e
rlau4 <p.lau.004@gmail.com>
2019-04-15 20:23:12 -0400

    added js to incorperate maps API

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,5 +1,38 @@
 // Google Places API
-    $(function activatePlacesSearch(){
-        var input = document.getElementById('searchBar');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-    });
+$(function activatePlacesSearch() {
+    var input = document.getElementById('searchBar');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+});
+
+//Map API call
+
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(
+      document.getElementById('map'), {center: sydney, zoom: 15});
+
+  var request = {
+    query: 'Museum of Contemporary Art Australia',
+    fields: ['name', 'geometry'],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
