a89a263e93f79b5175f7812cb426a2cf2e37b4cb
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-04-20 10:59:51 -0400

    recent search list populates

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -20,81 +20,126 @@ var inputCountry;
 var inputLat;
 var inputLong;
 
+var mf = 1;
+var m = 1;
+var item;  
+
 // Google Places API
 $(function activatePlacesSearch() {
-    var input = document.getElementById('searchBar');
+    var input = $('#searchBar');
     var autocomplete = new google.maps.places.Autocomplete(input);
 });
 
 // function searchInput() {
-    $("#submit").keyup(function() {
-    currentDest = $("searchBar").val();
-    $("searchBar").text('');
-    splitCurrentDest = currentDest.split(' ');
-    console.log(splitCurrentDest);
+$("#searchBar").keyup(function (e) {
+    currentDest = $("#searchBar").val();
 
-    //splits user input into strings for each word seperating city, state, and country
-    if (splitCurrentDest.length == 3) {
-        inputCity = splitCurrentDest[0];
-        inputState = splitCurrentDest[1];
-        inputCountry = splitCurrentDest[2];
-    } else if (splitCurrentDest.length == 2) {
-        inputCity = splitCurrentDest[0];
-        inputState = null;
-        inputCountry = splitCurrentDest[1];
-    } else if (splitCurrentDest.length == 4) {
-        inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1];
-        inputState = splitCurrentDest[2];
-        inputCountry = splitCurrentDest[3];
-    } else if (splitCurrentDest.length == 5) {
-       inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1] + " " + splitCurrentDest[2];
-       inputState = splitCurrentDest[3];
-       inputCountry = splitCurrentDest[4];
-    }
-
-    //variable for pushing to firebase
-    newSearch = {
-        city: inputCity,
-        state: inputState,
-        country: inputCountry,
-        // latitude: inputLat,
-        // longitude: inputLong
-    }
-
-  //push to firebase
-    data.ref().push(newSearch);
-    console.log("City: " + newSearch.city);
-    console.log("State " + newSearch.state);
-    console.log("country:" + newSearch.country);
-    popularArr.push(newSearch.city);
-});
-
-// $("#submit").keyup( "13", searchInput);
+    if (e.which == 13) {
 
 
-//populate top searches array and html list
-data.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
-    
-    arrayCity = snapshot.val().city;
-    arrayState = snapshot.val().state;
-    arrayCountry = snapshot.val().country;
-    
-    if (arrayState == null) {
-    popularArr.push(arrayCity + " " + arrayCountry);
-    } else { 
-        popularArr.push(arrayCity + " " + arrayState + " " + arrayCountry);
-    }
-  
-    for (i = 0; i < 5; i++) {
-        $("top-item").remove();
-        var popularDest = $("<p>").addClass("top-item").text(popularArr[i]);
-        $("#top-search").append(popularDest);
+        splitCurrentDest = currentDest.split(' ');
+        console.log(splitCurrentDest);
+
+        //splits user input into strings for each word seperating city, state, and country
+        if (splitCurrentDest.length == 3) {
+            inputCity = splitCurrentDest[0];
+            inputState = splitCurrentDest[1];
+            inputCountry = splitCurrentDest[2];
+        } else if (splitCurrentDest.length == 2) {
+            inputCity = splitCurrentDest[0];
+            inputState = null;
+            inputCountry = splitCurrentDest[1];
+        } else if (splitCurrentDest.length == 4) {
+            inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1];
+            inputState = splitCurrentDest[2];
+            inputCountry = splitCurrentDest[3];
+        } else if (splitCurrentDest.length == 5) {
+            inputCity = splitCurrentDest[0] + " " + splitCurrentDest[1] + " " + splitCurrentDest[2];
+            inputState = splitCurrentDest[3];
+            inputCountry = splitCurrentDest[4];
+        }
+
+        //variable for pushing to firebase
+        newSearch = {
+            city: inputCity,
+            state: inputState,
+            country: inputCountry,
+            // latitude: inputLat,
+            // longitude: inputLong
+        }
+//push to firebase
+        data.ref().push(newSearch);
+        // console.log("City: " + newSearch.city);
+        // console.log("State " + newSearch.state);
+        // console.log("country:" + newSearch.country);
+        popularArr.push(newSearch.city);
         popularArr = [];
+        
+        //populate top searches array and html list
+        $(".top-item").remove();
+        data.ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function (snapshot) {
+
+            arrayCity = snapshot.val().city;
+            arrayState = snapshot.val().state;
+            arrayCountry = snapshot.val().country;
+
+            if (arrayState == null) {
+                popularArr.push(arrayCity + " " + arrayCountry);
+            } else {
+                popularArr.push(arrayCity + " " + arrayState + " " + arrayCountry);
+            }
+
+            for (i = 0; i < 5; i++) {
+                var popularDest = $("<p>").addClass("top-item").attr("id", "city" + popularArr[i]).text(popularArr[i]);
+                $("#top-search").append(popularDest);
+                popularArr = [];
+                
+            }
+
+        });
+    
+        for (var i = 0; i < popularArr.length; i++) {
+            for (var i = 0; i < popularArr.length; i++) {
+                for (var j = i; j < popularArr.length; j++) {
+                    if (popularArr[i] == popularArr[j])
+                        m++;
+                    if (mf < m) {
+                        mf = m;
+                        item = popularArr[i];
+                    }
+                }
+                m = 0;
+            }
+        }
+        console.log(item + " " + mf);
+        console.log(popularArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
+    $("#searchBar").text('');
+
 });
 
+
+
+function recentSearchClick(){
+    currentDest = $(this).text();
+    console.log(currentDest);
+}
+$(document).on("click", ".top-item", recentSearchClick);
 //Map API call
 
 var map;
@@ -103,92 +148,92 @@ var infowindow;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
         zoomControl: true,
         scaleControl: true,
         fullscreenControl: true,
-      });
-      
+    });
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('searchBar');
-        var searchBox = new google.maps.places.SearchBox(input);
-       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('searchBar');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
 
-          if (places.length == 0) {
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
             return;
-          }
+        }
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+
             var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
             };
 
 
             if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
             } else {
-              bounds.extend(place.geometry.location);
+                bounds.extend(place.geometry.location);
             }
-          });
-          map.fitBounds(bounds);
         });
-      }
-
-
-$(document).ready(function() {
-    initMap();
-    
-});
-    // $(function activatePlacesSearch(){
-    //     var input = document.getElementById('searchBar');
-    //     var autocomplete = new google.maps.places.Autocomplete(input);
-    // });
-    // $(function activatePlacesSearch(){
-    //     var input = document.getElementById('searchBar2');
-    //     var autocomplete = new google.maps.places.Autocomplete(input);
-    // });
-// Original Search Bar
-    $(".btn1").click(function(){
-        $(".input").addClass("active").focus;
-        $(this).addClass("animate");
-        $(".input").val("");
-
+        map.fitBounds(bounds);
     });
+}
+
+
+$(document).ready(function () {
+    initMap();
+
+});
+// $(function activatePlacesSearch(){
+//     var input = document.getElementById('searchBar');
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+// });
+// $(function activatePlacesSearch(){
+//     var input = document.getElementById('searchBar2');
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+// });
+// Original Search Bar
+$(".btn1").click(function () {
+    $(".input").addClass("active").focus;
+    $(this).addClass("animate");
+    $(".input").val("");
+
+});
 
 // Hiding and showing elements on search
 $(".hidden").hide();
-$('#searchBar').keypress(function(event){
+$('#searchBar').keypress(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
-    
-    if(keycode == '13'){
+
+    if (keycode == '13') {
         $(".hidden").show();
         $(".show").hide();
     }
-    
+
 });
 
-$("#searchForm").submit(function(e) {
+$("#searchForm").submit(function (e) {
     e.preventDefault();
 });
 
@@ -208,11 +253,11 @@ $(document).ready(function () {
         $.ajax({
             url: searchUrl,
             method: 'GET'
-        }).then(function(response) {
+        }).then(function (response) {
             var pageElement = $('<div>');
-            
+
             if (response.thumbnail) pageElement.append($('<img>').attr('width', 150).attr('src', response.thumbnail.source));
-            
+
             pageElement.append($('<h2>').append($('<a>').text(response.title)));
 
             pageElement.append($('<p>').text(response.extract));
@@ -227,5 +272,5 @@ $(document).ready(function () {
         ajaxArticleData();
         console.log(input);
     });
-    
+
 })
