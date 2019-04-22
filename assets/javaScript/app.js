e475d7c89876ed014b6e1c4b172a039f36f1d75c
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-04-22 14:59:07 -0400

    wiki url fixed

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -27,18 +27,24 @@ var slicedCurrentCountry;
 var articles = $('.articles');
 var inputWiki = $('#searchBar').val().trim();
 var button = $('button');
-var searchUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+var searchUrl;
 
 var mf = 1;
 var m = 1;
 var item;
 var recentListsStart
 var mostRecentIndex
+
+var map;
+var service;
+var infowindow;
+var input;
+
 // Google Places API
-$(function activatePlacesSearch() {
+function activatePlacesSearch() {
     var input = $('#searchBar');
     var autocomplete = new google.maps.places.Autocomplete(input);
-});
+};
 
 // function searchInput() {
 $("#searchBar").keyup(function (e) {
@@ -48,7 +54,7 @@ $("#searchBar").keyup(function (e) {
 
 
         splitCurrentDest = currentDest.split(' ');
-        
+
         console.log(splitCurrentDest);
 
         //splits user input into strings for each word seperating city, state, and country
@@ -71,9 +77,11 @@ $("#searchBar").keyup(function (e) {
         }
 
         console.log(inputCity);
+        //removes "," from input for the WIKI API url
         slicedCurrentCity = inputCity.slice(0, inputCity.length - 1);
-        slicedCurrentState = inputState.slice(0, inputState.length - 1);
-        slicedCurrentCountry = inputCountry.slice(0, inputCountry.length - 1);
+        if (splitCurrentDest.length > 2) {
+            slicedCurrentState = inputState.slice(0, inputState.length - 1);
+        }
 
         //variable for pushing to firebase
         newSearch = {
@@ -84,167 +92,155 @@ $("#searchBar").keyup(function (e) {
             // latitude: inputLat,
             // longitude: inputLong
         }
-//push to firebase
+        //push to firebase
         data.ref().push(newSearch);
-        // console.log("City: " + newSearch.city);
-        // console.log("State " + newSearch.state);
-        // console.log("country:" + newSearch.country);
         popularArr.push(newSearch.city);
         popularArr = [];
 
-        //populate top searches array and html list
-        data.ref().orderByChild("dateAdded").limitToLast(200).on("child_added", function (snapshot) {
-            $(".top-item").remove();
 
-            arrayCity = snapshot.val().city;
-            arrayState = snapshot.val().state;
-            arrayCountry = snapshot.val().country;
-            if (arrayState == null) {
-                popularArr.push(arrayCity + " " + arrayCountry);
-            } else {
-                popularArr.push(arrayCity + " " + arrayState + " " + arrayCountry);
-            }
-        
-            var mostRecentIndex = popularArr.length;
-            var recentListsStart = mostRecentIndex - 5;
-            // var mostRecent = popularArr[mostRecentIndex];
-            // var popularDest = $("<div>").addClass("top-item").attr("id", "city" + mostRecent).text(mostRecent);
-            for (i = recentListsStart; i < mostRecentIndex; i++) {
-                var popularDest = $("<div>").addClass("top-item").attr("id", "city" + popularArr[i]).text(popularArr[i]);
-                $("#top-search").append(popularDest);
-            }
-
-        });
-        
-        for (var i = 0; i < popularArr.length; i++) {
-            for (var i = 0; i < popularArr.length; i++) {
-                for (var j = i; j < popularArr.length; j++) {
-                    if (popularArr[i] == popularArr[j])
-                    m++;
-                    if (mf < m) {
-                        mf = m;
-                        item = popularArr[i];
-                    }
-                }
-                m = 0;
-            }
-        }
+        popListPopulate();
         console.log(item + " " + mf);
         console.log(popularArr);
-        
+
     }
 
     $("#searchBar").text('');
-    searchUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/' + inputCity;
+    // searchUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/' + inputCity;
 });
 
+//populate top searches array and html list
+function popListPopulate() {
+    data.ref().orderByChild("dateAdded").limitToLast(200).on("child_added", function (snapshot) {
+        $(".top-item").remove();
+        searchUrl = "";
+        arrayCity = snapshot.val().city;
+        arrayState = snapshot.val().state;
+        arrayCountry = snapshot.val().country;
+        if (arrayState == null) {
+            popularArr.push(arrayCity + " " + arrayCountry);
+        } else {
+            popularArr.push(arrayCity + " " + arrayState + " " + arrayCountry);
+        }
 
+        var mostRecentIndex = popularArr.length;
+        var recentListsStart = mostRecentIndex - 5;
+        for (i = recentListsStart; i < mostRecentIndex; i++) {
+            var popularDest = $("<div>").addClass("top-item").attr("id", "city" + popularArr[i]).text(popularArr[i]);
+            $("#recent-search").append(popularDest);
+        }
 
-function recentSearchClick(){
+    });
+
+    for (var i = 0; i < popularArr.length; i++) {
+        for (var i = 0; i < popularArr.length; i++) {
+            for (var j = i; j < popularArr.length; j++) {
+                if (popularArr[i] == popularArr[j])
+                    m++;
+                if (mf < m) {
+                    mf = m;
+                    item = popularArr[i];
+                }
+            }
+            m = 0;
+        }
+    }
+}
+
+function recentSearchClick() {
     currentDest = $(this).text();
     console.log(currentDest);
-
-}
-$(document).on("click", ".top-item", recentSearchClick);
-//Map API call
-
-var map;
-var service;
-var infowindow;
-
-function initMap() {
+    $("#searchBar").val(currentDest);
+    popListPopulate();
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
         zoomControl: true,
         scaleControl: true,
         fullscreenControl: false,
-      });
-      
+    });
+    activatePlacesSearch();
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+}
+$(document).on("click", ".top-item", recentSearchClick);
+//Map API call
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('searchBar');
-        var searchBox = new google.maps.places.SearchBox(input);
-       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        zoomControl: true,
+        scaleControl: true,
+        fullscreenControl: false,
+    });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
 
-          if (places.length == 0) {
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('searchBar');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
             return;
-          }
+        }
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
 
             if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
             } else {
-              bounds.extend(place.geometry.location);
+                bounds.extend(place.geometry.location);
             }
-          });
-          map.fitBounds(bounds);       
         });
-        
-      }
+        map.fitBounds(bounds);
+    });
 
-
-$(document).ready(function() {
-    initMap();
+}
+function mapStuffsTest() {
     
-});
-    $(function activatePlacesSearch(){
-        var input = document.getElementById('searchBar');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-    });
-    $(function activatePlacesSearch(){
-        var input = document.getElementById('searchBar2');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-    });
-// Original Search Bar
-    $(".btn1").mouseover(function(){
-        $(".input").addClass("active").focus;
-        $(this).addClass("animate");
-        $(".input").val();
-        $("#button").hide();
-    });
+}
+initMap();
+activatePlacesSearch();
 
-    $(".btn1").click(function(){
-      $(".hidden").show();
-      $(".show").hide();
-  })
+// Original Search Bar
+$(".btn1").mouseover(function () {
+    $(".input").addClass("active").focus;
+    $(this).addClass("animate");
+    $(".input").val();
+    $("#button").hide();
+});
+
+$(".btn1").click(function () {
+    $(".hidden").show();
+    $(".show").hide();
+})
 
 // Hiding and showing elements on search
 $(".hidden").hide();
-$('#searchBar').keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    
-    if(keycode == '13'){
+$('#searchBar').keypress(function (e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
         $(".hidden").show();
         $(".show").hide();
     }
+    popListPopulate();
 });
 
-$("#searchForm").submit(function(e) {
+$("#searchForm").submit(function (e) {
     e.preventDefault();
 });
 
@@ -252,30 +248,34 @@ $("#searchForm").submit(function(e) {
 
 
 
+//wiki api
+function ajaxArticleData() {
+    if (inputState == null) {
+        searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + slicedCurrentCity + "%" + inputCountry + '&prop=info&format=jsonfm';
+    } else {
+        searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + slicedCurrentCity + "%" + slicedCurrentState + "%" + inputCountry + '&prop=info&format=jsonfm';
+    }
+    $.ajax({
+        url: searchUrl,
+        method: 'GET'
+    }).then(function (response) {
+        var pageElement = $('<div>');
 
-function ajaxArticleData () {
-    searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&titles='  + slicedCurrentCity + "%" + slicedCurrentState + "%" + slicedCurrentCountry + '&prop=info&format=jsonfm';
-  $.ajax({
-      url: searchUrl,
-      method: 'GET'
-  }).then(function(response) {
-      var pageElement = $('<div>');
-      
-      if (response.thumbnail) pageElement.append($('<img>').attr('width', 150).attr('src', response.thumbnail.source));
-      
-      pageElement.append($('<h2>').append($('<a>').text(response.title)));
+        if (response.thumbnail) pageElement.append($('<img>').attr('width', 150).attr('src', response.thumbnail.source));
 
-      pageElement.append($('<p>').text(response.extract));
+        pageElement.append($('<h2>').append($('<a>').text(response.title)));
 
-      pageElement.append($('<hr>'));
+        pageElement.append($('<p>').text(response.extract));
 
-      articles.append(pageElement);
-  })
+        pageElement.append($('<hr>'));
+
+        articles.append(pageElement);
+    })
 }
 
-$("#searchBar").on('keyup', function(e){
-  if (e.keyCode == 13) {
-    ajaxArticleData();
-    console.log(inputWiki);
-  }
+$("#searchBar").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+        ajaxArticleData();
+        console.log(inputWiki);
+    }
 })
